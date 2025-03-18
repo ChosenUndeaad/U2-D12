9f75879bb099865b215d54c7b0e6cfa67e4c589d
ChosenUndeaad <32023627+ChosenUndeaad@users.noreply.github.com>
2025-03-18 20:47:40 +0100

    Update

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
 const nameList = document.getElementById("name");
 const saveButton = document.getElementById("save");
 const deleteButton = document.getElementById("delete");
+const list = document.getElementById("list");
+
+let storedNames = [];
+try {
+  storedNames = JSON.parse(localStorage.getItem("names"));
+} catch (err) {} //se c'è un errore nel blocco try entri nel catch
+//C'è il blocco finally che viene eseguito ogni volta dopo try e catch anche dopo un catch
+
+const append = function (name) {
+  let p = document.createElement("p");
+  p.innerText = name;
+  list.appendChild(p);
+};
+
+storedNames.forEach((name) => {
+  append(name);
+});
 
 saveButton.addEventListener("click", function () {
-  const text = nameList.value;
-  localStorage.setItem("names", text);
+  storedNames.push(nameList.value);
+  localStorage.setItem("names", JSON.stringify(storedNames));
+  append(nameList.value);
+});
+
+deleteButton.addEventListener("click", function () {
+  storedNames.pop();
+  localStorage.setItem("names", JSON.stringify(storedNames));
+  list.removeChild(list.lastElementChild);
 });
